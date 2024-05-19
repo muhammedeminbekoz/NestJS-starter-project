3f8d348995e57711cc7bd9a8261701d5c72215d3
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-19 15:12:25 +0300

    feat: Add class-transformer and class-validator dependencies

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+}
