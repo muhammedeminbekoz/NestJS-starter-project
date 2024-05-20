3dd2d227e2c7ba727ebf489746d754c05074a475
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-20 23:59:10 +0300

    feat: Add User entity with basic fields

---
+++ src/user/dto/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
