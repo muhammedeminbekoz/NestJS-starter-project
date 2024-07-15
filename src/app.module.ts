4038961b310735ed0d85e3a760a561e441d15c6e
Emin Beköz <m.eminbekoz19@gmail.com>
2024-07-15 17:16:23 +0300

    refactor: Remove unused files and code related to the user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmOptions } from './config/typeorm.config';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [UserModule, TypeOrmModule.forRoot(typeOrmOptions)],
 })
 export class AppModule {}
