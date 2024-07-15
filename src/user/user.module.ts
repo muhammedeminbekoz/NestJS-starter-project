571b168ceca643fe50d421c88dcd908f095b868e
Emin Beköz <m.eminbekoz19@gmail.com>
2024-07-15 17:17:07 +0300

    Refactor:  user module imports The user module imports have been refactored to import the User entity from the correct file location.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './dto/entities/user.entity';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class UserModule {}
