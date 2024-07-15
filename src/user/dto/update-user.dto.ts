e6022ae40423953b40fe2703437b8580d9a23f5b
Emin Beköz <m.eminbekoz19@gmail.com>
2024-07-16 01:03:05 +0300

    feat: Add UpdateUserDto for updating user's name and surname

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
+}
