525c4b598cc465b541d6652046946b9b5c143c63
Emin Beköz <m.eminbekoz19@gmail.com>
2024-07-15 17:17:43 +0300

    feat: Add surname field to CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,22 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
+
   @IsEmail()
   email: string;
+
+  @IsStrongPassword()
+  password: string;
 }
